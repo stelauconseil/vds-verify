0ebf6dda453a4459596d4f0583f2229be9f2e393
Nicolas Chalanset <nicolas@chalanset.fr>
2023-12-16 00:00:00 +0100

    FIX typo

--- screens/Settings.js
+++ screens/Settings.js
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
     textAlign: "left",
     color: "grey",
     margin: 20,
-    fontVariant: ["small-caps"],
+    fontVariant: "small-caps",
   },
   listMiddle: {
     width: "90%",
